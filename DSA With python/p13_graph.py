b4ed05681e7d243de6a5918f3813fb64373dc8d5
Durjoy Barua <142112376+durjoy5327@users.noreply.github.com>
2024-07-29 23:49:03 +0600

    Add files via upload

---
+++ DSA With python/p13_graph.py
@@ -0,0 +1,20 @@
+class Graph:
+    def __init__(self, edges):
+        self.data=data
+
+
+
+
+
+if __name__=='main':
+
+    cities=[
+        ('mombai', 'paris'),
+        ('mombai','dubai'),
+        ('paris','dubai'),
+        ('paris', 'newwork'),
+        ('dubai','newwork' ),
+        ('newwork','toronto')
+
+
+    ]
