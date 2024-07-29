b1432efcc04077faba8fe992c5532318701996bb
Durjoy Barua <durjoybarua8115@example.com>
2024-07-30 00:54:02 +0600

    chane

--- DSA With python/p13_graph.py
+++ DSA With python/p13_graph.py
@@ -6,21 +6,52 @@ class Graph:
             if start in self.graph_dict:
                 self.graph_dict[start].append(end)
             else:
-                self.graph_dict[start]=end
+                self.graph_dict[start]=[end]
         print("Graph ", self.graph_dict)
 
+    def get_paths(self, start ,end, path=[]):
+        path=path+[start]
+        if start==end:
+            return [path]
+        if start not in self.graph_dict:
+            return []
 
+        paths=[]
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths= self.get_paths(node , end , path)
+                for p in new_paths:
+                    paths.append(p)
+        return paths
 
+    def get_shortest_path(self, start, end , path=[]):
+        path = path + [start]
+        if start==end:
+            return path
+        if start not in self.graph_dict:
+            return []
+        shortest_path= None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_path= self.get_shortest_path(node , end , path)
+                if new_path:
+                    if shortest_path is None or len(new_path)<len(shortest_path):
+                        shortest_path=new_path
 
+        return shortest_path
 
-if __name__=='main':
+if __name__=='__main__':
 
     routes=[
         ('mombai', 'paris'),
         ('mombai','dubai'),
         ('paris','dubai'),
-        ('paris', 'newwork'),
-        ('dubai','newwork' ),
-        ('newwork','toronto')
+        ('paris', 'newYork'),
+        ('dubai','newYork' ),
+        ('newYork','toronto')
     ]
     routes_graph= Graph(routes)
+    start="mombai"
+    end="newYork"
+    print(f"Paths between {start} to {end} :", routes_graph.get_paths(start, end))
+    print(f"Shortest path between {start} and {end} : ", routes_graph.get_shortest_path(start,end))
