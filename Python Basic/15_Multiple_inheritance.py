c588c6a4b523584f03909af5fa5825f6b933be01
Durjoy Barua <durjoybarua8115@example.com>
2024-08-02 12:45:17 +0600

    push

--- Python Basic/15_Multiple_inheritance.py
+++ Python Basic/15_Multiple_inheritance.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 """
 class Father:
@@ -11,7 +10,7 @@ class Mother:
 
 class child(Father, Mother):
     def sports(self):
-        print("I love sports")
+       print("I love sports")
 
 """
 class Father:
@@ -32,39 +31,4 @@ class child(Father , Mother):
 
 if __name__== '__main__':
     object = child()
-=======
-
-"""
-class Father:
-    def gardening(self):
-        print("I love gardening")
-
-class Mother:
-    def cooking(self):
-        print("I love cooking")
-
-class child(Father, Mother):
-    def sports(self):
-        print("I love sports")
-
-"""
-class Father:
-    def skills(self):
-        print("Gardening, Programming")
-
-class Mother:
-    def skills(self):
-        print("Cooking , Art")
-
-class child(Father , Mother):
-    def skills(self):
-        Father.skills(self)
-        Mother.skills(self)
-        print("Sketching, Programming, ")
-
-
-
-if __name__== '__main__':
-    object = child()
->>>>>>> 478502b706ea70c8a192a84e5e1c38cd0062e72e
     object.skills()
--- Python Basic/16_Exception_Handling_Advance.py
+++ Python Basic/16_Exception_Handling_Advance.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 """
 try:
@@ -6,6 +5,7 @@ try:
 except MemoryError as e:
     print(e)
 """
+
 
 
 
@@ -49,55 +49,3 @@ process_file()
 
 
 
-=======
-
-"""
-try:
-    raise MemoryError("Memory Error")
-except MemoryError as e:
-    print(e)
-"""
-
-
-
-
-
-#User define exception handling class
-"""
-class Accident(Exception):
-    def __init__(self, msg):
-        self.msg= msg
-    
-    def handle(self):
-        print("Accient occur take a detour")
-
-if __name__=='__main__':
-    try:
-        raise Accident("Two car crashes")
-    except Accident as e:
-        e.handle()
-
-"""
-
-
-
-#multiple error handling
-def process_file():
-    f = None 
-    try:
-        f = open("F:\\Python-Basic-and-DSA-Python-\\Python Basic\\info.txt")
-        x = 1 / 0 
-    except FileNotFoundError as e:
-        print("File not found inside problem")
-    finally:
-        print("There is another error cleaning up file")
-        if f:
-            f.close()
-
-process_file()
-
-
-
-
-
->>>>>>> 478502b706ea70c8a192a84e5e1c38cd0062e72e
